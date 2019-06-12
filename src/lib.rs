4bd1917903bd3ade9b3f55154b4cddc330143e99
Bryant Eisenbach <fubuloubu@gmail.com>
2019-06-12 17:54:00 -0400

    WIP: Filled it out a bit more

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Package {
+    // Required Members
+    manifest_version: String,
+    package_name: String,
+    version: String,
+    // Other members here...
+}
+
 #[cfg(test)]
 mod tests {
+    // Ensure Package structure conforms to the spec
+    // present in ethpm-spec/spec/package.spec.json
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn package_struct_meets_spec() {
+    }
+
+    // Try to parse packages located in
+    // ethpm-spec/examples/**/*.json
+    #[test]
+    fn can_parse_examples() {
     }
 }
