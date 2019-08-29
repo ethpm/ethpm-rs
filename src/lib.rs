b4adfbb158bebeb8c0b57854bcb009b918dd5f56
SRCoughlin <sean@froth.com>
2019-08-28 22:30:46 -0400

    Preparing merge

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,111 @@ extern crate serde;
 extern crate serde_json;
 
 use serde::{Serialize, Deserialize};
+//use http::Uri;
 
+//Rust-based transcription of Package standard as specified at https://github.com/ethpm/ethpm-spec/blob/master/spec/package.spec.json
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Package {
-    // Required Members
     manifest_version: String,
     package_name: String,
+    meta: Option<PackageMeta>,
     version: String,
-    // Other members here...
+    sources: Option<u8>,
+    contract_types: Option<ContractType>,
+    deployments: Option<Deployment>,
+    build_dependencies: Option<ContentURI>,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PackageMeta {
+    authors: Option<Vec<String>>,
+    license: Option<String>,
+    description: Option<String>,
+    keywords: Option<Vec<String>>,
+    links: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContractType {
+    contract_name: Option<String>,
+    deployment_bytecode: Option<BytecodeObject>,
+    runtime_bytecode: Option<BytecodeObject>,
+    abi: Option<Vec<u8>>,
+    natspec: Option<String>,
+    compiler: Option<CompilerInformation>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContractInstance {
+    contract_type: String,
+    address: Address,
+    transaction: Option<TransactionHash>,
+    block: Option<BlockHash>,
+    runtime_bytecode: Option<BytecodeObject>,
+    compiler: Option<CompilerInformation>,
+    link_dependencies: Option<Vec<LinkValue>>,
+}
+
+//ByteString: not used explicitly
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BytecodeObject {
+    bytecode: Option<String>,//as ByteString
+    link_references: Option<Vec<LinkReference>>,
+    link_dependencies: Option<Vec<LinkValue>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LinkReference {
+    offsets: Vec<i64>,
+    length: u64,
+    name: Identifier,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LinkValue {
+    offsets: Vec<i64>,
+    #[serde(rename = "type")]
+    json_type: String,
+    value: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Identifier {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContractInstanceName {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Deployment {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PackageContractInstanceName {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CompilerInformation {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Address {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TransactionHash {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BlockHash {
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContentURI {
+}
+
 
 #[cfg(test)]
 mod tests {
